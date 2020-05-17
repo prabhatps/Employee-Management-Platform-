b8e3a6bba703f23a7e17a47cfe8f179306808673
Prabhat Suman <prabhatsuman@hotmail.com>
2020-05-17 15:55:25 +0530

    Init Commit

---
+++ controllers/api.js
@@ -0,0 +1,46 @@
+const { promisify } = require('util');
+const cheerio = require('cheerio');
+
+const lob = require('lob')(process.env.LOB_KEY);
+const axios = require('axios');
+const validator = require('validator');
+
+
+
+/**
+ * GET /api
+ * List of API examples.
+ */
+exports.getApi = (req, res) => {
+  res.render('api/index', {
+    title: 'API Examples'
+  });
+};
+
+
+
+
+ 
+/**
+ * GET /api/upload
+ * File Upload API example.
+ */
+
+exports.getFileUpload = (req, res) => {
+  res.render('api/upload', {
+    title: 'File Upload'
+  });
+};
+
+exports.postFileUpload = (req, res) => {
+  req.flash('success', { msg: 'File was uploaded successfully.' });
+  res.redirect('/api/upload');
+};
+
+
+
+
+ 
+
+
+
